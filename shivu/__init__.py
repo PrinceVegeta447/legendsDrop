6fb72a33fce7417dd97d359d3e8d754d1e5751a8
PrinceVegeta447 <vegetakun447@gmail.com>
2025-02-07 13:24:03 +0530

    Update __init__.py

--- shivu/__init__.py
+++ shivu/__init__.py
@@ -33,7 +33,7 @@ OWNER_ID = Config.OWNER_ID
 
 application = Application.builder().token(TOKEN).build()
 shivuu = Client("Shivu", api_id, api_hash, bot_token=TOKEN)
-lol = AsyncIOMotorClient(mongo_url)
+lol = AsyncIOMotorClient(mongo_url, tls=True, tlsAllowInvalidCertificates=True)
 db = lol['Character_catcher']
 collection = db['anime_characters_lol']
 user_totals_collection = db['user_totals_lmaoooo']

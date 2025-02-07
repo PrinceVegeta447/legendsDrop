160ad50300b45c4d0bb3a7d2847b40973227a235
PrinceVegeta447 <vegetakun447@gmail.com>
2025-02-07 17:21:39 +0530

    Update changetime.py

--- shivu/modules/changetime.py
+++ shivu/modules/changetime.py
@@ -7,7 +7,7 @@ from pyrogram.types import Message
 ADMINS = [ChatMemberStatus.ADMINISTRATOR, ChatMemberStatus.OWNER]
 
 
-@shivuu.on_message(filters.command("changetime"))
+@shivuu.on_message(filters.command("set_droptime"))
 async def change_time(client: Client, message: Message):
     user_id = message.from_user.id
     chat_id = str(message.chat.id)  # Store chat_id as a string

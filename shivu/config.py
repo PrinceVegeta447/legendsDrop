b186147523dcc14f3c2ec0b9fa80206d2405638f
PrinceVegeta447 <vegetakun447@gmail.com>
2025-02-08 11:34:44 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -4,7 +4,7 @@ class Config(object):
     # Get this value from my.telegram.org/apps
     OWNER_ID = "1710597756"
     sudo_users = [1710597756]
-    GROUP_ID = -1002415126626
+    GROUP_ID = -1002483506913
     TOKEN = "7586298589:AAHNpgN-iWCazr_yYR4XFANCRfpH4ILl4qg"
     mongo_url = "mongodb+srv://vegetakun447:1jPSDznTX6gy7Nqr@cluster0.hcngy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     PHOTO_URL = ["http://ibb.co/WvZ6gLTr"]

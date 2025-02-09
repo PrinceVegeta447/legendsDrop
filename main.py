e7cd1b70c109c8394cf795cebe68b2abe206fb49
PrinceVegeta447 <vegetakun447@gmail.com>
2025-02-09 18:49:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -286,7 +286,7 @@ def main() -> None:
     # Add command handlers
     application.add_handler(CommandHandler(["guess", "protecc", "collect", "grab", "hunt"], guess, block=False))
     application.add_handler(CommandHandler("fav", fav, block=False))
-    application.add_handler(MessageHandler(filters.ALL, message_counter, block=False))
+    application.add_handler(MessageHandler(filters.ALL & ~filters.COMMAND, message_counter, block=False))
     
 
     # Start polling for Telegram bot commands

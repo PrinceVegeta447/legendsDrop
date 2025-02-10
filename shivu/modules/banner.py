cdb6b4024b631ff6affc296153f25aba7fbc665e
PrinceVegeta447 <vegetakun447@gmail.com>
2025-02-10 08:41:06 +0530

    Update banner.py

--- shivu/modules/banner.py
+++ shivu/modules/banner.py
@@ -1,5 +1,5 @@
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import CommandHandler, CallbackContext
+from telegram.ext import CommandHandler, CallbackContext, CallbackQueryHandler
 from shivu import application, banners_collection, OWNER_ID, sudo_users
 from bson import ObjectId
 
